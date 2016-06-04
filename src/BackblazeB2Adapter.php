b6c119f910ba1f20511c4a73ce4e8eb718c9a6a5
duxet <duxetlg@gmail.com>
2016-06-04 19:24:20 +0200

    Init

---
+++ src/BackblazeB2Adapter.php
@@ -0,0 +1,290 @@
+<?php namespace duxet\Flysystem\BackblazeB2;
+
+use ChrisWhite\B2\Client;
+use League\Flysystem\Adapter\AbstractAdapter;
+use League\Flysystem\Config;
+
+class BackblazeB2Adapter extends AbstractAdapter
+{
+    /**
+     * @var string Bucket name
+     */
+    protected $bucket;
+
+    /**
+     * @var Client Backblaze B2 Client
+     */
+    protected $client;
+
+    public function __construct($accountId, $applicationKey, $bucket)
+    {
+        $this->client = new Client($accountId, $applicationKey);
+    }
+
+    /**
+     * Write a new file.
+     *
+     * @param string $path
+     * @param string $contents
+     * @param Config $config Config object
+     *
+     * @return array|false false on failure file meta data on success
+     */
+    public function write($path, $contents, Config $config)
+    {
+        $this->client->upload([
+            'BucketName' => $this->bucket,
+            'FileName' => $path,
+            'Body' => $contents,
+        ]);
+    }
+
+    /**
+     * Write a new file using a stream.
+     *
+     * @param string $path
+     * @param resource $resource
+     * @param Config $config Config object
+     *
+     * @return array|false false on failure file meta data on success
+     */
+    public function writeStream($path, $resource, Config $config)
+    {
+        return $this->write($path, $resource, $config);
+    }
+
+    /**
+     * Update a file.
+     *
+     * @param string $path
+     * @param string $contents
+     * @param Config $config Config object
+     *
+     * @return array|false false on failure file meta data on success
+     */
+    public function update($path, $contents, Config $config)
+    {
+        return $this->write($path, $contents, $config);
+    }
+
+    /**
+     * Update a file using a stream.
+     *
+     * @param string $path
+     * @param resource $resource
+     * @param Config $config Config object
+     *
+     * @return array|false false on failure file meta data on success
+     */
+    public function updateStream($path, $resource, Config $config)
+    {
+        return $this->update($path, $resource, $config);
+    }
+
+    /**
+     * Rename a file.
+     *
+     * @param string $path
+     * @param string $newpath
+     *
+     * @return bool
+     */
+    public function rename($path, $newpath)
+    {
+        // TODO: Implement rename() method.
+    }
+
+    /**
+     * Copy a file.
+     *
+     * @param string $path
+     * @param string $newpath
+     *
+     * @return bool
+     */
+    public function copy($path, $newpath)
+    {
+        // TODO: Implement copy() method.
+    }
+
+    /**
+     * Delete a file.
+     *
+     * @param string $path
+     *
+     * @return bool
+     */
+    public function delete($path)
+    {
+        return $this->client->deleteFile([
+            'BucketName' => $this->bucket,
+            'FileName' => $path,
+        ]);
+    }
+
+    /**
+     * Delete a directory.
+     *
+     * @param string $dirname
+     *
+     * @return bool
+     */
+    public function deleteDir($dirname)
+    {
+        // TODO: Implement deleteDir() method.
+    }
+
+    /**
+     * Create a directory.
+     *
+     * @param string $dirname directory name
+     * @param Config $config
+     *
+     * @return array|false
+     */
+    public function createDir($dirname, Config $config)
+    {
+        return $this->update($dirname, '', $config);
+    }
+
+    /**
+     * Set the visibility for a file.
+     *
+     * @param string $path
+     * @param string $visibility
+     *
+     * @return array|false file meta data
+     */
+    public function setVisibility($path, $visibility)
+    {
+        // TODO: Implement setVisibility() method.
+    }
+
+    /**
+     * Check whether a file exists.
+     *
+     * @param string $path
+     *
+     * @return array|bool|null
+     */
+    public function has($path)
+    {
+        return (bool) $this->getFileFromPath($path);
+    }
+
+    /**
+     * Read a file.
+     *
+     * @param string $path
+     *
+     * @return array|false
+     */
+    public function read($path)
+    {
+        return $this->client->download([
+            'BucketName' => $this->bucket,
+            'FileName' => $path,
+        ]);
+    }
+
+    /**
+     * Read a file as a stream.
+     *
+     * @param string $path
+     *
+     * @return array|false
+     */
+    public function readStream($path)
+    {
+        return $this->read($path);
+    }
+
+    /**
+     * List contents of a directory.
+     *
+     * @param string $directory
+     * @param bool $recursive
+     *
+     * @return array
+     */
+    public function listContents($directory = '', $recursive = false)
+    {
+        // TODO: Implement listContents() method.
+    }
+
+    /**
+     * Get all the meta data of a file or directory.
+     *
+     * @param string $path
+     *
+     * @return array|false
+     */
+    public function getMetadata($path)
+    {
+        // TODO: Implement getMetadata() method.
+    }
+
+    /**
+     * Get all the meta data of a file or directory.
+     *
+     * @param string $path
+     *
+     * @return array|false
+     */
+    public function getSize($path)
+    {
+        $file = $this->getFileFromPath($path);
+        return $file ? $file->getSize() : false;
+    }
+
+    /**
+     * Get the mimetype of a file.
+     *
+     * @param string $path
+     *
+     * @return array|false
+     */
+    public function getMimetype($path)
+    {
+        $file = $this->getFileFromPath($path);
+        return $file ? $file->getType() : false;
+    }
+
+    /**
+     * Get the timestamp of a file.
+     *
+     * @param string $path
+     *
+     * @return array|false
+     */
+    public function getTimestamp($path)
+    {
+        // TODO: Implement getTimestamp() method.
+    }
+
+    /**
+     * Get the visibility of a file.
+     *
+     * @param string $path
+     *
+     * @return array|false
+     */
+    public function getVisibility($path)
+    {
+        // TODO: Implement getVisibility() method.
+    }
+
+    /**
+     * Get File object from given path.
+     *
+     * @param string $path
+     * @return \ChrisWhite\B2\File
+     */
+    protected function getFileFromPath($path)
+    {
+        return $this->client->getFile([
+            'BucketName' => $this->bucket,
+            'FileName' => $path,
+        ]);
+    }
+}
